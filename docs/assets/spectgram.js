18f7c2b9042300df1e438651b0d50197e66daeb3
Fabio <fabiogo@outlook.com>
2017-06-02 16:28:08 -0300

    new sample

--- docs/assets/spectgram.js
+++ docs/assets/spectgram.js
@@ -8,6 +8,55 @@ var WIDTH;
 var HEIGHT;
 start_web_audio();
 
+var button = document.createElement("INPUT");
+button.setAttribute("type", "button");
+button.setAttribute("value", "toggle beat");
+button.setAttribute("onclick", "toggle()");
+var high = document.createElement("INPUT");
+high.setAttribute("type", "range");
+high.setAttribute("max", 2000);
+high.setAttribute("min", 100);
+var low = document.createElement("INPUT");
+low.setAttribute("type", "range");
+low.setAttribute("max", 2000);
+low.setAttribute("min", 100);
+var bpm = document.createElement("INPUT");
+bpm.setAttribute("type", "range");
+bpm.setAttribute("max", 2000);
+bpm.setAttribute("min", 100);
+bpm.setAttribute("onchange", "require_update = 1");
+var duration = document.createElement("INPUT");
+duration.setAttribute("type", "range");
+duration.setAttribute("max", 1);
+duration.setAttribute("min", 0.2);
+duration.setAttribute("step", 0.1);
+function br(){return document.createElement("br");}
+document.body.prepend(button,br(),"high",br(),high,br(),"low",br(),low,br(),"duration",br(),duration,br(),"bpm",br(),bpm);
+
+function loop(){
+  if(!require_update) beat(parseInt(low.value),parseInt(high.value),audio_context.currentTime,parseFloat(duration.value)*curBpm/1000);
+  else updateInterval();
+}
+var interval = null;
+var curBpm = 0;
+function updateInterval(){
+  require_update = 0;
+  window.clearInterval(interval);
+  curBpm = 2100-parseInt(bpm.value);
+  interval = setInterval(loop,curBpm);
+  loop();
+}
+function toggle(){
+  if(!interval){
+    curBpm = 2100-parseInt(bpm.value);
+    interval = setInterval(loop,curBpm);
+    loop();
+  }
+  else {
+    window.clearInterval(interval);
+    interval = null;
+  }
+}
 
 function start_web_audio(){
   // Create some nodes...
@@ -24,9 +73,9 @@ function start_web_audio(){
   
   // Create Noisy node...
   n = new Noisy(audio_context, 16384); 
-  beat(440,3200,1); // This function creates noise in a range of the spectrum for a short time.
+  //beat(440,3200,1); // This function creates noise in a range of the spectrum for a short time.
   //harmonic(440,1); // This function uses Noisy as an oscillator, making only one bin active.
-  beat(440,440*3,1.5);
+  /*beat(440,440*3,1.5);
   beat(440,440*6,2);
   beat(493.88,493.88*3,2.5);
   beat(493.88,493.88*6,3);
@@ -39,7 +88,7 @@ function start_web_audio(){
   node.connect(g); // Noise processor won't start/stop, it gets connected and play. Control it with gain nodes.
   g.connect(destination);
   g.gain.setValueAtTime(1,5)
-
+*/
   // Draw spectrum...
   analyser = audio_context.createAnalyser();
   gain.connect(analyser);
@@ -57,17 +106,20 @@ function start_web_audio(){
   draw();
 }
 
-function beat(l, h, t){
+function beat(l, h, t, d){
+  if(d===undefined) d=1
+  if(l>h) l = h;
   var b = n.createNoise(l,h); // Create the node.
   b.start();
   var g = audio_context.createGain(); // Gain node simulating ADSR
   g.gain.value = 0;
   g.gain.linearRampToValueAtTime(0,t);
-  g.gain.linearRampToValueAtTime(1,t+0.05);
-  g.gain.linearRampToValueAtTime(0.2,t+0.15);
-  g.gain.linearRampToValueAtTime(0,t+0.50);
+  g.gain.linearRampToValueAtTime(1,t+0.10*d);
+  g.gain.linearRampToValueAtTime(0.2,t+0.30*d);
+  g.gain.linearRampToValueAtTime(0,t+d);
   b.connect(g); // Connect it, just like a regular node.
   g.connect(destination);
+  b.stop(t+d);
 }
 
 function harmonic(hz, t){
@@ -85,32 +137,21 @@ function harmonic(hz, t){
   h3.stop(t+1);
 }
 
-var a;
+var offset_x=0;
 function draw() {
-  canvas.clearRect(0, 0, WIDTH, HEIGHT);
   var freqDomain = new Uint8Array(analyser.frequencyBinCount);
-  a = freqDomain;
   analyser.getByteFrequencyData(freqDomain);
   var fac = 4;
+  var width = WIDTH * 0.001;
+  var height = HEIGHT/analyser.frequencyBinCount*fac;
   for (var i = 0; i < analyser.frequencyBinCount/fac; i++) {
     var value = freqDomain[i];
-    var percent = value / 256;
-    var height = HEIGHT * percent;
-    var offset = HEIGHT - height - 1;
-    var barWidth = WIDTH/analyser.frequencyBinCount*fac;
-    canvas.fillStyle = 'hsl( 1, 100%, 50%)';
-    canvas.fillRect(i * barWidth, offset, barWidth, height);
+    var offset_y = HEIGHT - height*i - 1;
+    var light = value/256 * 100;
+    canvas.fillStyle = 'hsl(0, 0%, '+ light +'%)';
+    canvas.fillRect(offset_x-0.3, offset_y, width, height);
   }
-  var timeDomain = new Uint8Array(analyser.frequencyBinCount);
-  analyser.getByteTimeDomainData(timeDomain);
-  for (var i = 0; i < analyser.frequencyBinCount; i++) {
-    var value = timeDomain[i];
-    var percent = value / 256;
-    var height = HEIGHT * percent;
-    var offset = HEIGHT - height - 1;
-    var barWidth = WIDTH/analyser.frequencyBinCount;
-    canvas.fillStyle = 'black';
-    canvas.fillRect(i * barWidth, offset, 1, 1);
-  }
+  offset_x=(offset_x+width)%WIDTH;
+  canvas.clearRect(offset_x, 0, width, HEIGHT);
   drawVisual = requestAnimationFrame(draw);
 }
---
+++ docs/sample2.html
@@ -0,0 +1,18 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Web Audio">
+
+    <title>Noisy test</title>
+    <link type="text/css" href="assets/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div id="main">
+      <canvas class="visualizer"></canvas> 
+    </div>
+
+    <script type="text/javascript" src="assets/noisy.min.js"></script>
+    <script type="text/javascript" src="assets/spectgram.js"></script>
+  </body>
+</html>
